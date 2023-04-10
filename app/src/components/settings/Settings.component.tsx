e2ea182de4231fdc4e462b4e984e3afdb247c990
Efe Harmankaya <82821119+efeharmankaya@users.noreply.github.com>
2023-04-10 12:10:31 -0400

    mui icons

--- app/src/components/settings/Settings.component.tsx
+++ app/src/components/settings/Settings.component.tsx
@@ -1,11 +1,22 @@
+import { Person } from "@mui/icons-material";
+import { Icon } from "@mui/material";
 
 export const Settings = () => {
     return (
         <div className="settings-container">
             <h1>Settings</h1>
+            <Icon>
+                <Person />
+            </Icon>
 
-            <img src="/person symbol.png" alt="Failed to load person symbol"></img>
-<img src="QuestionMark.png" alt="Failed to load Question Mark"></img>
+            <img
+                src="/person symbol.png"
+                alt="Failed to load person symbol"
+            ></img>
+            <img
+                src="QuestionMark.png"
+                alt="Failed to load Question Mark"
+            ></img>
         </div>
     );
 };
