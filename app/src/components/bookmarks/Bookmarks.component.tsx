d051da3ed936f2802abf30f62142b82c2da21d34
DanielSoldera <d.v.soldera@gmail.com>
2023-04-10 12:07:27 -0400

    made Bookmarks

--- app/src/components/bookmarks/Bookmarks.component.tsx
+++ app/src/components/bookmarks/Bookmarks.component.tsx
@@ -1,7 +1,53 @@
+import React from "react";
+import {useState} from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { bookmarksSelector, loggedInSelector } from "../../redux/auth/auth.selector";
+import { RecipeRow } from "../recipeRow/RecipeRow.component";
+
+
+function Bookmark(props : String) {
+
+}
+
 export const Bookmarks = () => {
+ const loggedIn = useSelector(loggedInSelector)
+    const bookmarks = useSelector(bookmarksSelector)
+
+    const [results, setResults] = useState([])
+
+useEffect(() => {
+    if(!loggedIn){return}
+    fetch('/get-recipes', {
+    method: 'POST',
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify({ids : bookmarks})
+})
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log("/get-recipes: ", data);
+        if (data.success) {
+            setResults(data.data)
+           
+        } else {
+            console.error("/login response error: ", data);
+        }
+    })
+}, [loggedIn])
+
+   
+
+
     return (
         <div className="bookmarks-container">
             <h1>Bookmarks</h1>
+            {results.length && <div className= "bookmarkList">
+            {results.map((recipe: any, i) => <RecipeRow key = {i}{...recipe}/>)}
+            </div> }
+
+
         </div>
     );
-};
+}
+
+export default Bookmarks;
--- app/src/components/settings/Settings.component.tsx
+++ app/src/components/settings/Settings.component.tsx
@@ -1,7 +1,11 @@
+
 export const Settings = () => {
     return (
         <div className="settings-container">
             <h1>Settings</h1>
+
+            <img src="/person symbol.png" alt="Failed to load person symbol"></img>
+<img src="QuestionMark.png" alt="Failed to load Question Mark"></img>
         </div>
     );
 };
