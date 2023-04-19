7c0e49c633360e9a0210d43ab4a94b2eb41f916d
DanielSoldera <d.v.soldera@gmail.com>
2023-04-19 19:59:16 -0400

    added backend to settings

--- app/src/components/changeImage/ChangeImage.component.tsx
+++ app/src/components/changeImage/ChangeImage.component.tsx
@@ -1,19 +1,67 @@
 import { getAuth } from "firebase/auth";
 import { Person } from "@mui/icons-material";
-import "./changeImage.css"
+import "./changeImage.css";
+import { User, login } from "../../redux/auth/auth.reducer";
+import { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TextField, Button } from "@mui/material";
+import { userSelector } from "../../redux/auth/auth.selector";
 export const ChangeImage = () => {
-    const auth = getAuth()
-    return (<div>
-        <h1>Change Image</h1>
-        <div className="change-photo-container">
-            <h2>Current Photo</h2>
-            <div className='user-photo-container'>
-  {auth.currentUser?.photoURL ? <img src={auth.currentUser.photoURL} alt="Sometheing went wrong"/> : <Person/>}
-</div>
-<h2>Upload New Photo Below:</h2>
-<form>
-<input type="file" accept="image/*"></input>
-<input type="submit" value="Submit"></input></form>
+  const auth = getAuth();
+  const [image, setImage] = useState("");
+
+  const user = useSelector(userSelector);
+
+  const dispatch = useDispatch();
+  const handleImageSubmit = useCallback(() => {
+    if (!image.length) return;
+    fetch("/set-photo-url", {
+      method: "POST",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({
+        uid: auth.currentUser?.uid,
+        photoURL: image,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("/set-photo-url: ", data);
+        if (data.success) {
+          // data.data has the result
+          dispatch(login(data.data as User));
+        } else {
+          console.error("/set-photo-url response error: ", data);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, [image]);
+
+  return (
+    <div>
+      <h1>Change Image</h1>
+      <div className="change-photo-container">
+        <h2>Current Photo</h2>
+        <div className="user-photo-container">
+          {user?.photoURL ? (
+            <img src={user?.photoURL} alt="Sometheing went wrong" />
+          ) : (
+            <Person />
+          )}
         </div>
-    </div>)
-}
+        <h2>Upload New Photo Below:</h2>
+
+        <TextField
+          size="small"
+          onChange={(e) => setImage(e.target.value)}
+        ></TextField>
+        <Button
+          variant="outlined"
+          className="submit"
+          onClick={() => handleImageSubmit()}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+};
--- app/src/components/changeName/ChangeName.component.tsx
+++ app/src/components/changeName/ChangeName.component.tsx
@@ -1,17 +1,48 @@
 import { getAuth } from "firebase/auth";
 import "./changeName.css";
+import { useCallback, useState } from "react";
+import { Button, TextField } from "@mui/material";
+import { userSelector } from "../../redux/auth/auth.selector";
+import { useDispatch, useSelector } from "react-redux";
+import { User, login } from "../../redux/auth/auth.reducer";
 export const ChangeName = () => {
+    const user = useSelector(userSelector);
+    const [newDisplayName, setNewDisplayName] = useState("");
     const auth = getAuth()
+    const dispatch = useDispatch()
+    const handleDisplayNameSubmit = useCallback(() => { 
+        if(!newDisplayName.length)return
+    fetch("/set-display-name", {
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify({
+            uid: auth.currentUser?.uid,
+            displayName: newDisplayName
+        }),
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log("/set-display-name: ", data);
+            if (data.success) {
+                // data.data has the result
+                dispatch(login(data.data as User))
+            } else {
+                console.error("/set-display-name response error: ", data);
+            }
+        })
+        .catch((err) => console.error(err));}, [newDisplayName]
+
+    )
     return(
         <div>
             <h1>Change Name</h1>
            <div className="name-container"> <h2>Current Name:</h2>
-            {auth.currentUser?.displayName}
+            {user?.displayName}
             <h2>Write New Name Below:</h2>
-            <form>
-            <input type="text" className="newName" id="newName"></input>
-            <input type="submit" value="Submit"></input>
-            </form>
+            
+            <TextField size="small" onChange={(e) => setNewDisplayName(e.target.value)}></TextField>
+            <Button variant="outlined" className="submit" onClick={() => handleDisplayNameSubmit()}>Submit</Button>
+            
             </div>
         </div>
     )
