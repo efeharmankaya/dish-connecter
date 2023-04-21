36c8731d70c334079f2cc821a52d52c48c5c975f
DanielSoldera <d.v.soldera@gmail.com>
2023-04-21 19:46:13 -0400

    profile fix

--- app/src/components/profile/Profile.component.tsx
+++ app/src/components/profile/Profile.component.tsx
@@ -1,19 +1,31 @@
 import "./profile.css"
 import { getAuth } from "firebase/auth";
 import { Person } from "@mui/icons-material";
+import { userSelector } from "../../redux/auth/auth.selector";
+import { useSelector } from "react-redux";
 
 export const Profile = () => {
-    const auth = getAuth()
+  const auth = getAuth();
+  const user = useSelector(userSelector);
 
-    
-    return(<div >
-<h1>Profile</h1>
-<br></br>
-<div className="profile-container">
-<h2>Details:</h2>
-<h3>Name:</h3> {auth.currentUser?.displayName}
-<h3>Email:</h3>{auth.currentUser?.email}
-<h3>Photo:</h3><div className='user-photo-container'>
-  {auth.currentUser?.photoURL ? <img src={auth.currentUser.photoURL} alt="Sometheing went wrong"/> : <Person/>}
-</div></div> </div>)
-}
+  return (
+    <div>
+      <h1>Profile</h1>
+      <br></br>
+      <div className="profile-container">
+        <h2>Details:</h2>
+        <h3>Name:</h3> {user?.displayName}
+        <h3>Email:</h3>
+        {auth.currentUser?.email}
+        <h3>Photo:</h3>
+        <div className="user-photo-container">
+          {user?.photoURL ? (
+            <img src={user?.photoURL} alt="Sometheing went wrong" />
+          ) : (
+            <Person />
+          )}
+        </div>
+      </div>{" "}
+    </div>
+  );
+};
